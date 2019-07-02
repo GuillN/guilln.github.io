94d172e3682b2ef7d1b9d46660cd309f0efea626
GuillN <gnaassan@gmail.com>
2019-07-03 00:07:13 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-My personal portfolio hosted on Github Pages.
-
-This project was created with [Create React App](https://github.com/facebook/create-react-app).
---
+++ precache-manifest.2a39274562801027a80dcfc4f648229a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e455b8667a2bda9a4bebbb10ff32fcc",
+    "url": "/static/media/darker-chevron-arrow-down.8e455b86.svg"
+  },
+  {
+    "revision": "af3e09c39ea3a70649c4",
+    "url": "/static/css/main.0145ca44.chunk.css"
+  },
+  {
+    "revision": "3cfb4a4f5ba210801eed",
+    "url": "/static/js/1.3cfb4a4f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "29d4766f9bf1261981dd805d8f3bde00",
+    "url": "/static/media/cv_fr.29d4766f.png"
+  },
+  {
+    "revision": "50dfc0d7cd75271e97675dfdb443a3cf",
+    "url": "/static/media/cv_fr.50dfc0d7.pdf"
+  },
+  {
+    "revision": "aee15bebdaf23a03bd0e89aff3438bd4",
+    "url": "/static/media/iseplogo.aee15beb.png"
+  },
+  {
+    "revision": "54e3b0e8584f45466b978e95e2329e50",
+    "url": "/static/media/curved.54e3b0e8.svg"
+  },
+  {
+    "revision": "46d1dddd75982d1fe74f14e0b962b4ad",
+    "url": "/static/media/mail.46d1dddd.svg"
+  },
+  {
+    "revision": "ec4ada8779f6fa7cf6558ab075ad7586",
+    "url": "/static/media/bluegithub.ec4ada87.svg"
+  },
+  {
+    "revision": "53a1301e823ccc5eb4d193c0ab44cdad",
+    "url": "/static/media/chevron-arrow-down.53a1301e.svg"
+  },
+  {
+    "revision": "af3e09c39ea3a70649c4",
+    "url": "/static/js/main.af3e09c3.chunk.js"
+  },
+  {
+    "revision": "dd46779f6b4d075c890457e6782c8b66",
+    "url": "/static/media/download.dd46779f.svg"
+  },
+  {
+    "revision": "968aaa24eeffdd779e4fc9c27c5cb730",
+    "url": "/static/media/france.968aaa24.svg"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/static/media/united-kingdom.e5564902.svg"
+  },
+  {
+    "revision": "51924b0ee72f5aa5cbacb13bd3f5006f",
+    "url": "/static/media/cv_en.51924b0e.png"
+  },
+  {
+    "revision": "53edafbd552e3fa213aef45576ac83b0",
+    "url": "/static/media/cv_en.53edafbd.pdf"
+  },
+  {
+    "revision": "fa0c5684c1b00aef9c7f2ec60aab19db",
+    "url": "/static/media/cnamlogo.fa0c5684.png"
+  },
+  {
+    "revision": "ec98e66da611feedbaecd2bfa3b1fd1c",
+    "url": "/static/media/cirruseo.ec98e66d.png"
+  },
+  {
+    "revision": "cc897495663989ec40c1d9e826097e71",
+    "url": "/static/media/appartoo.cc897495.png"
+  },
+  {
+    "revision": "2bdab533e2d90e4166737a55c7b7c782",
+    "url": "/static/media/darkest-chevron-arrow-down.2bdab533.svg"
+  },
+  {
+    "revision": "3cb2dc7f975186787930e7b93ee18dc0",
+    "url": "/static/media/Cocon-RegularTR.3cb2dc7f.ttf"
+  },
+  {
+    "revision": "fc9bb6354ec33a7dfcf1d59d63fc69ea",
+    "url": "/index.html"
+  }
+];
