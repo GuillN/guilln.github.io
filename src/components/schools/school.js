2b1f48871a70cc5eb7e58217bcac3605a265913b
GuillN <gnaassan@gmail.com>
2020-03-21 22:39:23 +0100

    css fix

--- src/components/schools/school.js
+++ src/components/schools/school.js
@@ -6,26 +6,27 @@ const School = props => {
         <div>
             <p className="text">
                 {props.date}<br/>
-                {props.school === "" ? <p>{props.fullName}</p> : <p>{props.school}: {props.fullName}</p>}<br/>
+                {props.school === "" ? <p>{props.fullName}</p> : <p>{props.school}: {props.fullName}</p>}
                 {props.cycle === "" ? "" : <p>{props.cycle}</p>}
             </p>
             <a target="_blank" rel="noopener noreferrer" href={props.href}>
                 <img src={props.src} alt={`${props.name}_logo`} className={`${props.name}-logo`}/>
             </a>
             <p className="text small">{props.subj}</p>
-            {props.subj2 === "" ? "" : <p className="text-small">{props.subj2}</p>}
+            {props.subj2 === "" ? "" : <p className="text small">{props.subj2}</p>}
             {
                 props.internship === "" ? "" :
                     <div>
+                        <br/>
                         <hr className="small-line"/>
-                        <p>{props.internship}: {props.internshipName.toUpperCase()}</p>
+                        <p className="text small">{props.internship}: {props.internshipName.toUpperCase()}</p>
                         <div className="internship-container">
                             <a target="_blank" rel="noopener noreferrer" href={props.internshipHref}>
                                 <img src={props.internshipSrc} alt={`${props.internshipName}_logo`} className="internship-logo"/>
                             </a>
                             <p className="internship-text">
                                 {props.internshipText.map(item => {
-                                    return <div><p>{item}</p><br/></div>
+                                    return <p>{item}</p>
                                 })}
                             </p>
                         </div>
