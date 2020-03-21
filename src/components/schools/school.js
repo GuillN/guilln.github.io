ae4c4ed98a92e8a45b1771d599f82bca83b41ddb
GuillN <gnaassan@gmail.com>
2020-03-21 22:33:53 +0100

    school comp upgrade

--- src/components/schools/school.js
+++ src/components/schools/school.js
@@ -1,18 +1,36 @@
 import React from "react"
+import cirruseo from "../../images/cirruseo.png";
 
 const School = props => {
     return (
         <div>
             <p className="text">
                 {props.date}<br/>
-                {props.school}: {props.fullName}<br/>
-                {props.cycle}
+                {props.school === "" ? <p>{props.fullName}</p> : <p>{props.school}: {props.fullName}</p>}<br/>
+                {props.cycle === "" ? "" : <p>{props.cycle}</p>}
             </p>
             <a target="_blank" rel="noopener noreferrer" href={props.href}>
                 <img src={props.src} alt={`${props.name}_logo`} className={`${props.name}-logo`}/>
             </a>
             <p className="text small">{props.subj}</p>
-            <p className="text-green">{props.subj2}</p>
+            {props.subj2 === "" ? "" : <p className="text-small">{props.subj2}</p>}
+            {
+                props.internship === "" ? "" :
+                    <div>
+                        <hr className="small-line"/>
+                        <p>{props.internship}: {props.internshipName.toUpperCase()}</p>
+                        <div className="internship-container">
+                            <a target="_blank" rel="noopener noreferrer" href={props.internshipHref}>
+                                <img src={props.internshipSrc} alt={`${props.internshipName}_logo`} className="internship-logo"/>
+                            </a>
+                            <p className="internship-text">
+                                {props.internshipText.map(item => {
+                                    return <div><p>{item}</p><br/></div>
+                                })}
+                            </p>
+                        </div>
+                    </div>
+            }
         </div>
     )
 };
--- src/components/schools/schools.js
+++ src/components/schools/schools.js
@@ -14,7 +14,8 @@ const Schools = props => {
             <h2 className="school-title">Formation</h2>
             <School date="2013 - 2016" school={props.strings.school_isep} cycle={props.strings.cycle}
                     fullName="Institut Supérieur d'Électronique de Paris" href="https://www.isep.fr/"
-                    src={isep} name="isep" subj={props.strings.subjects} subj2={props.strings.subjects2}/>
+                    src={isep} name="isep" subj={props.strings.subjects} subj2={props.strings.subjects2}
+                    internship="" internshipName="" internshipHref="" internshipSrc="" internshipText=""/>
             {/*<p className="text">*/}
             {/*    2013 - 2016<br/>*/}
             {/*    {props.strings.school_isep}: Institut Supérieur d'Électronique de Paris<br/>*/}
@@ -26,28 +27,42 @@ const Schools = props => {
             {/*<p className="text small">{props.strings.subjects}</p>*/}
             {/*<p className="text-green">{props.strings.subjects2}</p>*/}
             <hr className="line"/>
-            <p className="text">
-                2016 - 2017<br/>
-                Conservatoire National des Arts et Métiers
-            </p>
-            <a target="_blank" rel="noopener noreferrer" href="http://www.cnam.fr/">
-                <img src={cnam} alt="cnam_logo" className="cnam-logo"/>
-            </a>
-            <p className="text-green">
-                {props.strings.subjects3}<br/>
-                <hr className="small-line"/>
-                {props.strings.internship}: Cirruseo
-            </p>
-            <div className="internship-container">
-                <a target="_blank" rel="noopener noreferrer" href="https://www.cirruseo.com/">
-                    <img src={cirruseo} alt="cirruseo_logo" className="internship-logo"/>
-                </a>
-                <p className="internship-text">
-                    Google Script<br/>
-                    Google Cloud Engine<br/>
-                    NodeJS
-                </p>
-            </div>
+
+            <School date="2016 - 2017" school="" cycle=""
+                    fullName="Conservatoire National des Arts et Métiers" href="http://www.cnam.fr/"
+                    src={cnam} name="cnam" subj={props.strings.subjects3} subj2="" internship={props.strings.internship}
+                    internshipName="cirruseo" internshipHref="https://www.cirruseo.com/" internshipSrc={cirruseo}
+                    internshipText={["Google Script", "Google Cloud Platform", "NodeJS"]}/>
+
+
+            {/*<p className="text">*/}
+            {/*    2016 - 2017<br/>*/}
+            {/*    Conservatoire National des Arts et Métiers*/}
+            {/*</p>*/}
+            {/*<a target="_blank" rel="noopener noreferrer" href="http://www.cnam.fr/">*/}
+            {/*    <img src={cnam} alt="cnam_logo" className="cnam-logo"/>*/}
+            {/*</a>*/}
+            {/*<p className="text-green">*/}
+            {/*    {props.strings.subjects3}<br/>*/}
+            {/*    <hr className="small-line"/>*/}
+            {/*    {props.strings.internship}: Cirruseo*/}
+            {/*</p>*/}
+            {/*<div className="internship-container">*/}
+            {/*    <a target="_blank" rel="noopener noreferrer" href="https://www.cirruseo.com/">*/}
+            {/*        <img src={cirruseo} alt="cirruseo_logo" className="internship-logo"/>*/}
+            {/*    </a>*/}
+            {/*    <p className="internship-text">*/}
+            {/*        Google Script<br/>*/}
+            {/*        Google Cloud Engine<br/>*/}
+            {/*        NodeJS*/}
+            {/*    </p>*/}
+            {/*</div>*/}
+
+
+
+
+
+
             <hr className="line"/>
             <p className="text">
                 2017 - 2019<br/>
